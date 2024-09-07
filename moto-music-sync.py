2827eb21f50a45bd121b38cca991a431a831145d
Boris Glavic <lordpretzel@gmail.com>
2024-09-07 09:59:12 -0500

    Add initial files

---
+++ moto-music-sync.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env -S nix develop . --command python
+
+"""
+Sync music from Apple music playlist to Android device with adb
+"""
+
+__author__ = 'Boris Glavic'
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
